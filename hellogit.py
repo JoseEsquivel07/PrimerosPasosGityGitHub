9514c3a7ed7b96d4df360667227e671497dc0dc1
JoseEsquivel07 <joseesquivel@uadec.edu.mx>
2023-07-27 23:11:02 -0600

    comit al master del block poust en suver as reciente

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,4 @@
 #para que te enamores
 # ya volivi del pasado ahora guerdo con git checkout hash y archivo
+
+# se realizan modificaciones a los archiovos apra hecer otra rama y al final hacer merge
