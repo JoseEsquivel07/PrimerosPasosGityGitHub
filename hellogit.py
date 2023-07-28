4b12eca3eba90cd5f979c0ae5308ab3404f18deb
JoseEsquivel07 <joseesquivel@uadec.edu.mx>
2023-07-27 23:15:52 -0600

    estructura o comentario agregado en cabecera

--- hellogit.py
+++ hellogit.py
@@ -2,3 +2,5 @@
 # ya volivi del pasado ahora guerdo con git checkout hash y archivo
 
 # se realizan modificaciones a los archiovos apra hecer otra rama y al final hacer merge
+
+#realizandomodificaciones en rama cabecera
