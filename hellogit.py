4cc1bde59368161d175c26a0e2965f71a37009b2
JoseEsquivel07 <joseesquivel@uadec.edu.mx>
2023-07-27 23:20:23 -0600

    se guerdan los datos, me voy a bañar y a dormir

--- hellogit.py
+++ hellogit.py
@@ -4,3 +4,6 @@
 # se realizan modificaciones a los archiovos apra hecer otra rama y al final hacer merge
 
 #realizandomodificaciones en rama cabecera
+#para el cambio en rramas usar el comando git checkout (nombre de la rama)
+#para guerdar cambio en rama git add (nombre de repo)}
+#y hacer un commit con git commit -m (comentario de comit) o git commit -am
