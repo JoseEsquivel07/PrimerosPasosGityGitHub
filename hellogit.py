19614f5aca2ca9a227ed5b752beccbb626490ec7
JoseEsquivel07 <joseesquivel@uadec.edu.mx>
2023-07-28 07:39:45 -0600

    un commit al main

--- hellogit.py
+++ hellogit.py
@@ -2,3 +2,4 @@
 # ya volivi del pasado ahora guerdo con git checkout hash y archivo
 
 # se realizan modificaciones a los archiovos apra hecer otra rama y al final hacer merge
+#aciendo comit a la rama master 
