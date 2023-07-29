f4ca8d8e27ae3ab2880a5d73a08608d58bbbc659
JoseEsquivel07 <joseesquivel@uadec.edu.mx>
2023-07-28 23:03:31 -0600

    cambio para hacer merghe con el mismo contenido

--- hellogit.py
+++ hellogit.py
@@ -11,3 +11,4 @@
 #apunto de unit las dos ramas en la master con comando merge
 #git branch para mostrar las distintas ramas que se rienen en el proyecro
 #git merge (rama actual, la que se va a unir a la main)
+#para moverse ente commits o ramas usar comando git checkout (nombre de rama o hash de commit)
